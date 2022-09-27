e60ca8556118cb6954eb3528b3aa6cfcf46597c8
Antonio F. Gasco <antoniofgasco@gmail.com>
2022-09-27 11:21:46 +0100

    final commit

--- src/recipes/RecipeSearchPage.js
+++ src/recipes/RecipeSearchPage.js
@@ -1,3 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BackButton } from '../ui';
+import { RecipeSearchResultsList } from './RecipeSearchResultsList';
+import { useIngredients } from '../ingredients';
+import { useRecipeSearchResults } from './useRecipeSearchResults';
 
-export const RecipeSearchPage = () => <h1>Recipe SEarch Page</h1>;
+export const RecipeSearchPage = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchString, setSearchString] = useState('');
+  const { ingredients } = useIngredients();
+  const { searchResults } = useRecipeSearchResults(searchString);
+
+  const onSearchClicked = () => {
+    setSearchString(searchInput);
+  };
+
+  return (
+    <div className="page">
+      <BackButton />
+      <div className="centered-container">
+        <h1>Add Meal to Plan</h1>
+        <input
+          type="text"
+          className="full-width space-before space-after"
+          placeholder="Enter keyword here"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button className="full-width space-after" onClick={onSearchClicked}>
+          Search
+        </button>
+        <RecipeSearchResultsList
+          recipes={searchResults}
+          ingredients={ingredients}
+        />
+      </div>
+    </div>
+  );
+};
--- src/recipes/RecipeSearchResultsList.js
+++ src/recipes/RecipeSearchResultsList.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { RecipeSearchResultsListItem } from './RecipeSearchResultsListItem';
 
-export const RecipeSearchResultsList = () => (
-  <h1>Recipe Search Results List</h1>
+export const RecipeSearchResultsList = ({ ingredients, recipes }) => (
+  <>
+    {recipes.map((recipe) => (
+      <RecipeSearchResultsListItem
+        key={recipe.id}
+        recipe={recipe}
+        ingredients={ingredients}
+      />
+    ))}
+  </>
 );
